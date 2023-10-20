a1684cfce65d558bd14f8ce4d5885d5722feb2fb
Kunj Maheshwari <kunj.maheshwari2021@vitbhopal.ac.in>
2023-10-20 14:39:43 +0530

    Notes Application

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App(){
+    return <div>
+        <Header></Header>
+        <Note></Note>
+        <Footer></Footer>
+    </div>
+}
+
+export default App;
